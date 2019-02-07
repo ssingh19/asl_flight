87fcb48f4d865b315dbbc276bb7c92d4dc5ef99b
ssingh19 <sumeet.aussie@gmail.com>
2019-02-06 17:17:24 -0800

    flight experiments changes

--- src/tf2pose.cpp
+++ src/tf2pose.cpp
@@ -14,6 +14,7 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 
+
 /*!
  * Creates and runs the robot_pose_publisher node.
  *
@@ -40,47 +41,84 @@ int main(int argc, char ** argv)
   nh_priv.param<bool>("is_stamped", is_stamped, false);
 
   if(is_stamped)
-    p_pub = nh.advertise<geometry_msgs::PoseStamped>("robot_pose", 1);
+    p_pub = nh.advertise<geometry_msgs::PoseStamped>("/mavros/vision_pose/pose", 5);
   else
-    p_pub = nh.advertise<geometry_msgs::Pose>("robot_pose", 1);
+    p_pub = nh.advertise<geometry_msgs::Pose>("/mavros/vision_pose/pose", 5);
 
   // create the listener
   tf::TransformListener listener;
   listener.waitForTransform(map_frame, base_frame, ros::Time(), ros::Duration(1.0));
 
+  // create value placeholders
+  double pos_x, pos_y, pos_z = 0.0;
+  double att_w, att_x, att_y, att_z = 0.0;
+  att_w = 1.0;
+  tf::StampedTransform transform;
+
+  geometry_msgs::PoseStamped pose_stamped;
+
+  // artifical noise gen
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(0.0,0.01);
+  double rand_x, rand_y, rand_z = 0.0;
+
+
   ros::Rate rate(publish_frequency);
+
+  // continuously loop
   while (nh.ok())
   {
-    tf::StampedTransform transform;
     try
     {
       listener.lookupTransform(map_frame, base_frame, ros::Time(0), transform);
 
-      // construct a pose message
-      geometry_msgs::PoseStamped pose_stamped;
-      pose_stamped.header.frame_id = map_frame;
-      pose_stamped.header.stamp = ros::Time::now();
+      // if successful, store the values
+      pos_x = transform.getOrigin().getX();
+      pos_y = transform.getOrigin().getY();
+      pos_z = transform.getOrigin().getZ();
 
-      pose_stamped.pose.orientation.x = transform.getRotation().getX();
-      pose_stamped.pose.orientation.y = transform.getRotation().getY();
-      pose_stamped.pose.orientation.z = transform.getRotation().getZ();
-      pose_stamped.pose.orientation.w = transform.getRotation().getW();
+      att_w = transform.getRotation().getW();
+      att_x = transform.getRotation().getX();
+      att_y = transform.getRotation().getY();
+      att_z = transform.getRotation().getZ();
 
-      pose_stamped.pose.position.x = transform.getOrigin().getX();
-      pose_stamped.pose.position.y = transform.getOrigin().getY();
-      pose_stamped.pose.position.z = transform.getOrigin().getZ();
-
-      if(is_stamped)
-        p_pub.publish(pose_stamped);
-      else
-        p_pub.publish(pose_stamped.pose);
     }
     catch (tf::TransformException &ex)
     {
-      // just continue on
+      // use last recorded values
+      ROS_INFO("didn't see vicon tf!");
+
+      rand_x = distribution(generator);
+      rand_y = distribution(generator);
+      rand_z = distribution(generator);
+
+      pos_x += rand_x;
+      pos_y += rand_y;
+      pos_z += rand_z;
+
     }
 
+    // ALWAYS PUBLISH SOMETHING!!
+
+    pose_stamped.header.frame_id = map_frame;
+    pose_stamped.header.stamp = ros::Time::now();
+
+    pose_stamped.pose.orientation.x = att_x;
+    pose_stamped.pose.orientation.y = att_y;
+    pose_stamped.pose.orientation.z = att_z;
+    pose_stamped.pose.orientation.w = att_w;
+
+    pose_stamped.pose.position.x = pos_x;
+    pose_stamped.pose.position.y = pos_y;
+    pose_stamped.pose.position.z = pos_z;
+
+    if(is_stamped)
+      p_pub.publish(pose_stamped);
+    else
+      p_pub.publish(pose_stamped.pose);
+
     rate.sleep();
+
   }
 
   return EXIT_SUCCESS;
